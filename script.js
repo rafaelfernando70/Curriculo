93db56b8465829502e04ab5139e95c756eef4e50
rafaelfernando70 <fernando.silva.rafael@escola.pr.gov.br>
2026-02-13 11:07:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    document.body.style.opacity = 0;
+    setTimeout(function() {
+        document.body.style.transition = "opacity 1s";
+        document.body.style.opacity = 1;
+    }, 100);
+};
